75079c603a2179da287007003822ebb67ef7b1b1
Maxime Traverse <maxime.Traverse@orange.fr>
2021-09-13 20:53:17 +0200

    make compose template & its get method to access it

--- app.js
+++ app.js
@@ -25,6 +25,9 @@ app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent})
 })
 
+app.get("/compose", function(req, res){
+  res.render("compose")
+})
 
 app.listen(port, function() {
   console.log(`Server started on port ${port}`)
